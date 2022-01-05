db1d6b3762fb75038304e6f8de5bf52ddfa258ca
whitebear <woojs0505@naver.com>
2022-01-05 22:47:07 +0900

    Study ResponseBody

--- hello-spring/src/main/java/com/example/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/com/example/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.hellospring.controller;
         import org.springframework.stereotype.Controller;
         import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,39 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // @ResponseBody 사용 원리
+    // viewResolver 대신 HttpMessageConverter가 동작함 (기본 문자 처리 및 객체 처리)
+    @GetMapping("hello-string")
+    @ResponseBody()
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody()
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
