dae1df42cd2368322e0e5981ac993d793ca351ca
whitebear <woojs0505@naver.com>
2022-01-07 21:50:46 +0900

    [ADD] 히원가입

--- hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,19 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
---
+++ hello-spring/src/main/java/com/example/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.example.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
