375cda48e248325730504b315a36a17494dddd5f
whitebear <woojs0505@naver.com>
2022-01-08 09:18:22 +0900

    [ADD] 회원 목록 조회

--- hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,5 +32,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
