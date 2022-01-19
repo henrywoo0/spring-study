40a2d6658e460af3a8c464dfeba6728d5dd0c1a6
whitebear <woojs0505@naver.com>
2022-01-19 19:56:03 +0900

    Settings

---
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package hello.core.xml;
+
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
