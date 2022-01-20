0da695b8baed2c2ca4f3c76f1694eb2954fe3a73
whitebear <woojs0505@naver.com>
2022-01-20 22:34:14 +0900

    Test Lombok

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
