ef0cf3bb3a9098477a40fd8210b0b099c041989d
whitebear <woojs0505@naver.com>
2022-01-21 20:42:54 +0900

    setting thymeleaf

--- jpashop/src/main/java/jpabook/jpashop/Hello.java
+++
@@ -1,9 +0,0 @@
-package jpabook.jpashop;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class Hello {
-    private String data;
-}
--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
