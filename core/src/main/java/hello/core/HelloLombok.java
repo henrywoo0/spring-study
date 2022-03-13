20eeb04fb5bf6e96cc9fc3435e6fffe078f80f20
whitebear <woojs0505@naver.com>
2022-03-13 19:01:53 +0900

    Study lombok

---
+++ core/src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("하하");
+
+        String name = helloLombok.getName();
+        System.out.println("name = " + name);
+    }
+}
---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "경숙", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order).isEqualTo(1000);
+    }
+
+}
