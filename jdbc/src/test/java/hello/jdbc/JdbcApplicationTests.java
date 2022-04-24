13e8f9f2f5683f54f04aa6be79960603fd9a5772
우준성 <woojs0505@naver.com>
2022-04-24 10:23:26 +0900

    init

---
+++ jdbc/src/test/java/hello/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
