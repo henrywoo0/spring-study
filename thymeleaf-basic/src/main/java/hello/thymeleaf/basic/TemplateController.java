afdc6f40719cde51a54d74d42ffcb86473e2a2b2
우준성 <woojs0505@naver.com>
2022-05-25 17:35:50 +0900

    :tada: :: form project to study Spring MVC

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -13,4 +13,14 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
 }
