14b7ca48d9739d82dcc3cb47c84c8621edd5b94e
whitebear05 <woojs0505@naver.com>
2022-11-30 16:08:57 +0900

    :tada: :: 멀티모듈 프로젝트 세팅

---
+++ architecture/multimodule/core/src/test/java/com/example/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
