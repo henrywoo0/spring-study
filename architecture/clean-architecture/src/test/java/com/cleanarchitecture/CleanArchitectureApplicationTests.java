eeb957ae407234d4816faaa43662b219fac1a458
whitebear05 <woojs0505@naver.com>
2023-03-29 10:21:45 +0900

    :tada: :: start clean architecture study (in Java/Spring)

---
+++ architecture/clean-architecture/src/test/java/com/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
